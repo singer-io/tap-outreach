812ad50a217f79db2d89750b2b02bc953b405bfd
Andrew Madonna <andrew.w.madonna@gmail.com>
2019-08-15 13:18:49 -0400

    Add stream config. More streams, and switch by replication type

--- tap_outreach/sync.py
+++ tap_outreach/sync.py
@@ -6,8 +6,54 @@ from tap_outreach.discover import discover
 
 LOGGER = singer.get_logger()
 
-STREAM_PATHS = {
-    'stages': 'stages'
+STEAM_CONFIGS = {
+    'call_dispositions': {
+        'url_path': 'callDispositions',
+        'replication': 'incremental',
+        'filter_field': 'updatedAt',
+        'fks': ['creatorId']
+    },
+    'call_purposes': {
+        'url_path': 'callPurposes',
+        'replication': 'incremental',
+        'filter_field': 'updatedAt',
+        'fks': ['creatorId']
+    },
+    'duties': {
+        'url_path': 'duties',
+        'replication': 'full'
+    },
+    'events': {
+        'url_path': 'events',
+        'replication': 'incremental',
+        'filter_field': 'eventAt',
+        'fks': ['prospectId', 'userId']
+    },
+    'mailboxes': {
+        'url_path': 'mailboxes',
+        'replication': 'incremental',
+        'filter_field': 'updatedAt',
+        'fks': ['creatorId', 'updaterId']
+    },
+    'stages': {
+        'url_path': 'stages',
+        'replication': 'incremental',
+        'filter_field': 'updatedAt',
+        'fks': ['creatorId', 'updaterId']
+    },
+    'users': {
+        'url_path': 'users',
+        'replication': 'incremental',
+        'filter_field': 'updatedAt',
+        'fks': [
+            'calendarId',
+            'mailboxId',
+            'profileId',
+            'roleId',
+            'creatorId',
+            'updaterId'
+        ]
+    }
 }
 
 def get_bookmark(state, stream_name, default):
@@ -23,7 +69,7 @@ def write_schema(stream):
     schema = stream.schema.to_dict()
     singer.write_schema(stream.tap_stream_id, schema, stream.key_properties)
 
-def process_records(stream, mdata, max_modified, records):
+def process_records(stream, mdata, max_modified, records, filter_field, fks):
     schema = stream.schema.to_dict()
     with metrics.record_counter(stream.tap_stream_id) as counter:
         for record in records:
@@ -31,10 +77,33 @@ def process_records(stream, mdata, max_modified, records):
                 'id': record['id']
             }
             for prop, value in record['attributes'].items():
+                if prop == 'id':
+                    raise Exception('Error flattening Outeach record - conflict with `id` key')
                 record_flat[prop] = value
 
-            if record_flat['updatedAt'] > max_modified:
-                max_modified = record_flat['updatedAt']
+            if 'relationships' in record:
+                for prop, value in record['relationships'].items():
+                    if 'data' not in value and 'links' not in value:
+                        raise Exception('Only `data` or `links` expected in relationships')
+
+                    fk_field_name = '{}Id'.format(prop)
+
+                    if 'data' in value and fk_field_name in fks:
+                        data_value = value['data']
+                        if data_value is not None and 'id' not in data_value:
+                            raise Exception('null or `id` field expected for `data` relationship')
+
+                        if fk_field_name in record_flat:
+                            raise Exception(
+                                '`{}` exists as both an attribute and generated relationship name'.format(fk_field_name))
+
+                        if data_value == None:
+                            record_flat[fk_field_name] = None
+                        else:
+                            record_flat[fk_field_name] = data_value['id']
+
+            if filter_field in record_flat and record_flat[filter_field] > max_modified:
+                max_modified = record_flat[filter_field]
 
             with Transformer() as transformer:
                 record_typed = transformer.transform(record_flat,
@@ -52,20 +121,27 @@ def sync_endpoint(client, catalog, state, start_date, stream, mdata):
 
     write_schema(stream)
 
+    stream_config = STEAM_CONFIGS[stream_name]
+    filter_field = stream_config.get('filter_field')
+    fks = stream_config.get('fks', [])
+
     count = 1000
     offset = 0
     has_more = True
     max_modified = last_datetime
+    paginate_datetime = last_datetime
     while has_more:
         query_params = {
-            'filter[updatedAt]': '{}..inf'.format(last_datetime),
-            'sort': 'updatedAt',
             'page[limit]': count,
             'page[offset]': offset
         }
 
+        if stream_config.get('replication') == 'incremental':
+            query_params['filter[{}]'.format(filter_field)] = '{}..inf'.format(paginate_datetime)
+            query_params['sort'] = filter_field
+
         data = client.get(
-            STREAM_PATHS[stream_name],
+            stream_config['url_path'],
             params=query_params,
             endpoint=stream_name)
         records = data['data']
@@ -75,9 +151,19 @@ def sync_endpoint(client, catalog, state, start_date, stream, mdata):
         else:
             offset += count
 
-        max_modified = process_records(stream, mdata, max_modified, records)
+        max_modified = process_records(stream,
+                                       mdata,
+                                       max_modified,
+                                       records,
+                                       filter_field,
+                                       fks)
 
-        write_bookmark(state, stream_name, max_modified)
+        if offset > 10000:
+            paginate_datetime = max_modified
+            offset = 0
+
+        if stream_config.get('replication') == 'incremental':
+            write_bookmark(state, stream_name, max_modified)
 
 def update_current_stream(state, stream_name=None):  
     set_currently_syncing(state, stream_name) 
@@ -85,7 +171,7 @@ def update_current_stream(state, stream_name=None):
 
 def sync(client, catalog, state, start_date):
     if not catalog:
-        catalog = discover(client)
+        catalog = discover()
         selected_streams = catalog.streams
     else:
         selected_streams = catalog.get_selected_streams(state)
