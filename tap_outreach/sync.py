bf8b92dac3b141f630701d51050f0d7dc9554ce4
shantanu73 <shantanudhiman25@gmail.com>
2023-10-03 16:23:43 +0000

    Fixed attribute & generated relationship name issue with mailings stream for followUpSequenceId attribute.

--- tap_outreach/sync.py
+++ tap_outreach/sync.py
@@ -251,7 +251,7 @@ def process_records(stream, mdata, max_modified, records, filter_field, fks):
                                 'null or `id` field expected for `data` relationship')
 
                         # potential fix for the issue - https://github.com/singer-io/tap-outreach/issues/20
-                        if stream.tap_stream_id not in ["prospects", "events"]:
+                        if stream.tap_stream_id not in ["prospects", "events", "mailings"]:
                             if fk_field_name in record_flat:
                                 raise Exception('`{}` exists as both an attribute and generated relationship name'.format(fk_field_name))
 
